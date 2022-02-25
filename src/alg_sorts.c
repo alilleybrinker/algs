4b0700879e68de8bb70d7e8970025ee1de20cfbd
Andrew Lilley Brinker <andrewabrinker@gmail.com>
2022-02-25 10:29:26 -0800

    Removed excess logging.
    The logging logic is no longer needed. The resulting code is now
    shorter and easier to read.

--- src/alg_sorts.c
+++ src/alg_sorts.c
@@ -5,14 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef DO_LOG
-// By default, we don't log things.
-#define DO_LOG false
-#endif
-
-// Track the number of operations done in a sorting algorithm.
-static unsigned int N_OPS = 0;
-
 void selection_sort(char *, const size_t);
 void insertion_sort(char *, const size_t);
 void shell_sort(char *, const size_t);
@@ -24,33 +16,16 @@ static void exchange(char *, const size_t, const size_t);
 static size_t min_index(const char *, const size_t, const size_t);
 
 void selection_sort(char *s, const size_t len) {
-  N_OPS = 0;
-
   for (size_t i = 0; i < len; ++i) {
-    if (DO_LOG)
-      printf("%s\n", s);
     exchange(s, i, min_index(s, i, len));
   }
 }
 
 void insertion_sort(char *s, const size_t len) {
-  N_OPS = 0;
-
-  if (DO_LOG)
-    printf("%s\n", s);
-
   for (size_t i = 1; i < len; ++i) {
     for (size_t j = i; j > 0; --j) {
-      if (DO_LOG) {
-        N_OPS += 1;
-        printf("comp: %c %c (%u)\n", s[j], s[j - 1], N_OPS);
-      }
-
       if (s[j] < s[j - 1]) {
         exchange(s, j, j - 1);
-        if (DO_LOG)
-          printf("%s\n", s);
-
       } else {
         break;
       }
@@ -59,8 +34,6 @@ void insertion_sort(char *s, const size_t len) {
 }
 
 void shell_sort(char *s, const size_t len) {
-  N_OPS = 0;
-
   /**
    * This code is adapted from the pseudocode in the Wikipedia article
    * for Shell sort. Strangely, most other resources for implementing Shell
@@ -74,10 +47,6 @@ void shell_sort(char *s, const size_t len) {
   const size_t gaps[] = {5, 3, 1};
   const size_t n_gaps = sizeof(gaps) / sizeof(gaps[0]);
 
-  if (DO_LOG) {
-    printf("%s\n", s);
-  }
-
   for (size_t g = 0; g < n_gaps; ++g) {
     const size_t gap = gaps[g];
 
@@ -87,18 +56,7 @@ void shell_sort(char *s, const size_t len) {
         size_t j = i;
 
         while (j >= gap && s[j - gap] > temp) {
-          if (DO_LOG) {
-            N_OPS += 1;
-            printf("comp: %c %c (%u)\n", temp, s[j - gap], N_OPS);
-            N_OPS += 1;
-            printf("swap: %c %c (%u)\n", s[j], s[j - gap], N_OPS);
-          }
-
           s[j] = s[j - gap];
-
-          if (DO_LOG) {
-            printf("%s\n", s);
-          }
 
           j -= gap;
         }
@@ -110,8 +68,6 @@ void shell_sort(char *s, const size_t len) {
 }
 
 void bubble_sort(char *s, const size_t len) {
-  N_OPS = 0;
-
   bool swapped;
   size_t n = len;
 
@@ -119,11 +75,6 @@ void bubble_sort(char *s, const size_t len) {
     swapped = false;
 
     for (size_t i = 1; i < n; ++i) {
-      if (DO_LOG) {
-        N_OPS += 1;
-        printf("comp: %c %c (%u)\n", s[i - 1], s[i], N_OPS);
-      }
-
       if (s[i - 1] > s[i]) {
         exchange(s, i - 1, i);
         swapped = true;
@@ -143,11 +94,6 @@ void quick_sort(char *s, const size_t len) {
 }
 
 static void exchange(char *a, const size_t i1, const size_t i2) {
-  if (DO_LOG) {
-    N_OPS += 1;
-    printf("swap: %c %c (%u)\n", a[i1], a[i2], N_OPS);
-  }
-
   char tmp = a[i1];
   a[i1] = a[i2];
   a[i2] = tmp;
@@ -157,11 +103,6 @@ static size_t min_index(const char *a, const size_t s, const size_t len) {
   size_t v = s;
 
   for (size_t i = s + 1; i < len; ++i) {
-    if (DO_LOG) {
-      N_OPS += 1;
-      printf("comp: %c %c (%u)\n", a[i], a[v], N_OPS);
-    }
-
     if (a[i] < a[v])
       v = i;
   }
